0858270bc55d9204e0e95acd928e28bc514b49f3
koding user <aleostax@gmail.com>
2015-01-30 02:42:10 +0000

    First Draft of Begginer Python Course by Mark Lassoff @ learntoprogram.tv

---
+++ pythonCommands.py
@@ -0,0 +1,6 @@
+print "string\tEscaped Tab\nEscaped NewLine"
+print "Comments use hash signs instead of //"
+print "Variables\nname = \"nameString\"\t#comment"
+print "Use commas versus + to concatenate strings"
+print "Python uses whitespace for tree branch code"
+
